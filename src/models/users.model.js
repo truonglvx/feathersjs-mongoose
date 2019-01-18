da8734a98048f62bf34bbb0e644df06f2d143dfe
doronnahum <doron.nahum@gmail.com>
2019-01-18 11:06:55 +0200

    Add Authentication Service

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,15 +1,20 @@
 // users-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-module.exports = function (app) {
+module.exports = function(app) {
   const mongooseClient = app.get('mongooseClient');
-  const { Schema } = mongooseClient;
-  const users = new Schema({
-    text: { type: String, required: true }
-  }, {
-    timestamps: true
-  });
+  const users = new mongooseClient.Schema(
+    {
+      email: { type: String, unique: true, lowercase: true },
+      password: { type: String },
+      auth0Id: { type: String },
+      googleId: { type: String },
+      facebookId: { type: String },
+      githubId: { type: String }
+    },
+    { timestamps: true }
+  );
 
   return mongooseClient.model('users', users);
 };

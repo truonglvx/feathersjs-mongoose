e097aede0b51f96add083624f30eccc9a87a2f26
doronnahum <doron.nahum@gmail.com>
2019-01-19 21:51:04 +0200

    Make swagger more useful

--- src/services/posts/posts.service.js
+++ src/services/posts/posts.service.js
@@ -2,6 +2,7 @@
 const createService = require('feathers-mongoose');
 const createModel = require('../../models/posts.model');
 const hooks = require('./posts.hooks');
+const modelToSwagger = require('../../utils/swagger.util');
 
 module.exports = function (app) {
   const Model = createModel(app);
@@ -12,11 +13,13 @@ module.exports = function (app) {
     paginate
   };
 
+  const postsService = createService(options);
+  postsService.docs = modelToSwagger(Model);
+
   // Initialize our service with any options it requires
-  app.use('/posts', createService(options));
+  app.use('/posts', postsService);
 
   // Get our initialized service so that we can register hooks
   const service = app.service('posts');
-
   service.hooks(hooks);
 };

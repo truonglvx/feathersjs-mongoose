c483d7d323682705e55787ab00a2e7b6f4c618c5
doronnahum <doron.nahum@gmail.com>
2019-01-22 10:11:07 +0200

    Create Mailer services

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const users = require('./users/users.service.js');
 const posts = require('./posts/posts.service.js');
 const uploads = require('./uploads/uploads.service.js');
 const files = require('./files/files.service.js');
+const mailer = require('./mailer/mailer.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(posts);
   app.configure(uploads);
   app.configure(files);
+  app.configure(mailer);
 };

c424530abe8caad949968383854c20e765a8d070
doronnahum <doron.nahum@gmail.com>
2019-01-29 12:40:07 +0200

    create pick.test.js

--- src/utils/pick.js
+++ src/utils/pick.js
@@ -1,6 +1,7 @@
 const validate = require('validate.js');
 const sift = require('sift').default;
 const {deletePropertyPath} = require('./helpers');
+const cloneDeep = require('lodash.clonedeep');
 
 const can = function(data, conditions) {
   const _sift = sift(conditions);
@@ -35,7 +36,8 @@ const getFieldProperties = function(field){
  * console.log(pick(testObject, ['id', 'user.name', 'comments.body'])); // Get only specific field
  *  console.log(pick(testObject, ['-user.name'])); // Get all values except user.name
  */
-const pick = function(data, _fields){
+const pick = function(_data, _fields){
+  const data = cloneDeep(_data);
   let values = {};
   let isFieldsNegative = false;
   
@@ -119,76 +121,79 @@ const pick = function(data, _fields){
 module.exports = pick;
 
 
-// const testObject = {
-//   '_id': 4,
-//   'userId':'aak',
-//   'user': {
-//     'id': 'aak',
-//     'name': 'Leanne Graham',
-//     'username': 'Bret',
-//     'email': 'Sincere@april.biz',
-//     'address': {
-//       'street': 'Kulas Light',
-//       'suite': 'Apt. 556',
-//       'city': 'Gwenborough',
-//       'zipcode': '92998-3874',
-//       'geo': {
-//         'lat': '-37.3159',
-//         'lng': '81.1496'
-//       }
-//     },
-//     'phone': '1-770-736-8031 x56442',
-//     'website': 'hildegard.org',
-//     'company': {
-//       'name': 'Romaguera-Crona',
-//       'catchPhrase': 'Multi-layered client-server neural-net',
-//       'bs': 'harness real-time e-markets'
+// const comments = [
+//   {
+//     'postId': 1,
+//     'id': 1,
+//     'name': 'id labore ex et quam laborum',
+//     'email': 'Eliseo@gardner.biz',
+//     'body': 'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium'
+//   },
+//   {
+//     'postId': 1,
+//     'id': 2,
+//     'name': 'quo vero reiciendis velit similique earum',
+//     'email': 'Jayne_Kuhic@sydney.com',
+//     'body': 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et'
+//   },
+//   {
+//     'postId': 1,
+//     'id': 3,
+//     'name': 'odio adipisci rerum aut animi',
+//     'email': 'Nikita@garfield.biz',
+//     'body': 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione'
+//   },
+//   {
+//     'postId': 1,
+//     'id': 4,
+//     'name': 'alias odio sit',
+//     'email': 'Lew@alysha.tv',
+//     'body': 'non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati'
+//   },
+//   {
+//     'postId': 1,
+//     'id': 5,
+//     'name': 'vero eaque aliquid doloribus et culpa',
+//     'email': 'Hayden@althea.biz',
+//     'body': 'harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et'
+//   }
+// ];
+// const user = {
+//   'id': 'aak',
+//   'name': 'Leanne Graham',
+//   'username': 'Bret',
+//   'email': 'Sincere@april.biz',
+//   'address': {
+//     'street': 'Kulas Light',
+//     'suite': 'Apt. 556',
+//     'city': 'Gwenborough',
+//     'zipcode': '92998-3874',
+//     'geo': {
+//       'lat': '-37.3159',
+//       'lng': '81.1496'
 //     }
 //   },
+//   'phone': '1-770-736-8031 x56442',
+//   'website': 'hildegard.org',
+//   'company': {
+//     'name': 'Romaguera-Crona',
+//     'catchPhrase': 'Multi-layered client-server neural-net',
+//     'bs': 'harness real-time e-markets'
+//   }
+// };
+// const testData = {
+//   '_id': 4,
+//   'userId':'aak',
+//   'user': user,
 //   'title': 'eum et est occaecati',
 //   'body': 'ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit',
-//   'comments': [
-//     {
-//       'postId': 1,
-//       'id': 1,
-//       'name': 'id labore ex et quam laborum',
-//       'email': 'Eliseo@gardner.biz',
-//       'body': 'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium'
-//     },
-//     {
-//       'postId': 1,
-//       'id': 2,
-//       'name': 'quo vero reiciendis velit similique earum',
-//       'email': 'Jayne_Kuhic@sydney.com',
-//       'body': 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et'
-//     },
-//     {
-//       'postId': 1,
-//       'id': 3,
-//       'name': 'odio adipisci rerum aut animi',
-//       'email': 'Nikita@garfield.biz',
-//       'body': 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione'
-//     },
-//     {
-//       'postId': 1,
-//       'id': 4,
-//       'name': 'alias odio sit',
-//       'email': 'Lew@alysha.tv',
-//       'body': 'non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati'
-//     },
-//     {
-//       'postId': 1,
-//       'id': 5,
-//       'name': 'vero eaque aliquid doloribus et culpa',
-//       'email': 'Hayden@althea.biz',
-//       'body': 'harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et'
-//     }
-//   ]
+//   'comments': comments
 // };
+
 // console.log(pick(testObject, ['id', 'user.name', 'comments.body']));
 // console.log(pick(testObject, ['-user.name']));
 // console.log(pick(testObject, ['_id',{'user': {'user.id': 'aak'}}]));
 // console.log(pick(testObject, ['_id',{'user': {'user.id': 'WRONGaak'}}]));
 // console.log(pick(testObject, ['_id',{'user': {'_id': '4'}}]));
-// console.log(pick(testObject, ['_id',{'user.name': {'_id': 4}}]));
+// console.log(pick(testData, ['_id',{'user.name': {'_id': 74}}]));
 // console.log(pick(testObject, ['-_id',{'-user': {userId: 'aak'}}]));
